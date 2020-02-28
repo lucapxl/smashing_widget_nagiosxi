9be4adeea7c06519fa5ba95b27601a465986467e
Luca Sari <lsari@netability.local>
2020-02-28 15:18:05 +0800

    fixed many bugs with returns and return types

--- jobs/nagiosxi.rb
+++ jobs/nagiosxi.rb
@@ -1,114 +1,199 @@
 require 'open-uri'
 require 'json'
+require 'nokogiri'
 
-#nagios api key
 apiKey = 'your nagios api key here'
-nagiosIP = 'your nagios XI server IP'
+nagiosHOST = 'your nagios XI host hostname here'
+ 
+hostsURL = 'http://' + nagiosHOST + '/nagiosxi/api/v1/objects/hoststatus?apikey=' + apiKey
+servicesURL = 'http://' + nagiosHOST + '/nagiosxi/api/v1/objects/servicestatus?apikey=' + apiKey
 
-hostsURL = 'http://' + nagiosIP + '/nagiosxi/api/v1/objects/hoststatus?apikey=' + apiKey
-servicesURL = 'http://' + nagiosIP + '/nagiosxi/api/v1/objects/servicestatus?apikey=' + apiKey
+#nagios hosts codes: 0=ok, 1=down, 2=unreachable
+#nagios services codes: 0=ok, 1=warning, 2=critical, 3=unknown
+#smashing nagios status codes: 4=critical, 3=warning, 2=unknown, 1=scheduled, 0=acknowledged
 
-
-SCHEDULER.every '60', :first_in => 0 do
-  resp = Net::HTTP.get_response(URI.parse(hostsURL))
-  jsonData = resp.body
-  hostsStatus = JSON.parse(jsonData)
-
-  resp2 = Net::HTTP.get_response(URI.parse(servicesURL))
-  jsonData2 = resp2.body
-  servicesStatus = JSON.parse(jsonData2)
-
-  nagios_hosts_count = [0,0,0,0]
-  nagios_services_count = [0,0,0,0,0]
-
-  #status for hosts will be: 0=ok, 1=pending, 2=unreachable, 3=down
-  nagios_hosts_highest_status = 0;
-  
-  #status for services will be: 0=ok, 1=pending, 2=unknown, 3=warning, 4=critical
-  nagios_services_highest_status = 0;
-
-  nagios_hosts_message = ''
-  nagios_services_message = ''
-
-  if (hostsStatus['hoststatus'].respond_to? :each or servicesStatus['servicestatus'].respond_to? :each)
-
-    hostsStatus['hoststatus'].each do |child|
-      case child['current_state']
-      when "0" #up
-        nagios_hosts_count[0] += 1
-      when "1" #down
-        nagios_hosts_count[1] += 1
-        nagios_hosts_message += "<p class='single-message-header'><i class='fa fa-exclamation-circle'></i><span class='servername'>" + child['alias'] + "</span><span class='detail'>HOST DOWN</span></p>"
-        if nagios_hosts_highest_status < 3
-          nagios_hosts_highest_status = 3
-        end
-      when "2" #unreachable
-        nagios_hosts_count[2] += 1
-        nagios_hosts_message += "<p class='single-message-header'><i class='fa fa-exclamation-triangle'></i><span class='servername'>" + child['alias'] + "</span><span class='detail'>HOST UNRECHABLE</span></p>"
-        if nagios_hosts_highest_status < 2
-          nagios_hosts_highest_status = 2
-        end
-      else #pending
-        nagios_hosts_count[3] += 1
-        nagios_hosts_message += "<p class='single-message-header'><i class='fa fa-question'></i><span class='servername'>" + child['alias'] + "</span><span class='detail'>HOST PENDING</span></p>"
-        if nagios_hosts_highest_status < 1
-          nagios_hosts_highest_status = 1
-        end
-      end
+def getServiceLevel(state,scheduled,acknowledged)
+    returnStatus = 2
+    case state
+    when 1 #warning
+        returnStatus = 3
+    when 2 #critical
+        returnStatus = 4
+    when 3 #unknown
+        returnStatus = 2
+    when 4 #host down
+        returnStatus = 4
+    else 
+        returnStatus = 2
     end
 
-    servicesStatus['servicestatus'].each do |child|
-      case child['current_state']
-      when "0" #ok
-        nagios_services_count[0] += 1
-      when "1" #warning
-        nagios_services_count[1] += 1
-        nagios_services_message += "<p class='single-message-header'><i class='fa fa-exclamation-triangle'></i><span class='servername'>" + child['host_alias'] + "</span><span class='detail'>" + child['display_name'] + "</span></p>"
-        nagios_services_message += "<p class='single-message-body'>" + child['status_text'] + "</p>"
-        if nagios_services_highest_status < 3
-          nagios_services_highest_status = 3
-        end
-      when "2" #critical
-        nagios_services_count[2] += 1
-        nagios_services_message += "<p class='single-message-header'><i class='fa fa-exclamation-circle'></i><span class='servername'>" + child['host_alias'] + "</span><span class='detail'>" + child['display_name'] + "</span></p>"
-        nagios_services_message += "<p class='single-message-body'>" + child['status_text'] + "</p>"
-        if nagios_services_highest_status < 4
-          nagios_services_highest_status = 4
-        end
-      when "3" #unknown
-        nagios_services_count[3] += 1
-        nagios_services_message += "<p class='single-message-header'><i class='fa fa-question'></i><span class='servername'>" + child['host_alias'] + "</span><span class='detail'>" + child['display_name'] + "</span></p>"
-        nagios_services_message += "<p class='single-message-body'>" + child['status_text'] + "</p>"          
-        if nagios_services_highest_status < 2
-          nagios_services_highest_status = 2
-        end
-      else #pending
-        nagios_services_count[4] += 1
-        if nagios_services_highest_status < 1
-          nagios_services_highest_status = 1
-        end
-     end
+    if acknowledged
+        returnStatus = 0
+    elsif scheduled
+        returnStatus = 1
     end
 
-    hoststatus = nagios_hosts_highest_status == 3 ? "red" : (nagios_hosts_highest_status == 2 ? "yellow" : (nagios_hosts_highest_status > 0 ? "grey" : "green"))
-    servicestatus = nagios_services_highest_status == 4 ? "red" : (nagios_services_highest_status == 3 ? "yellow" : (nagios_services_highest_status > 0 ? "grey" : "green"))
+    return returnStatus
+end
 
-    if nagios_services_count[0] == 0 and nagios_services_count[1] == 0 and nagios_hosts_count[0] == 0 and nagios_hosts_count[1] == 0
-      if servicesStatus.length == 0 or hostsStatus.length == 0
-        hoststatus = "error"
-        servicestatus = "error"
-      end
+def getLevelIcon(status)
+    case status
+    when 0 #acknowledged
+        return 'fa-briefcase'
+    when 1 #scheduled
+        return 'fa-clock-o'
+    when 2 #unknown
+        return 'fa-question'
+    when 3 #warning
+        return 'fa-exclamation-triangle'
+    when 4 #critical
+        return 'fa-exclamation-circle'
     end
+    return 'fa-question'
+end
 
-    send_event('nagiosxihosts', { host_up: nagios_hosts_count[0], host_down: nagios_hosts_count[1], host_unreachable: nagios_hosts_count[2], host_pending: nagios_hosts_count[3], hoststatus: hoststatus })
-    send_event('nagiosxiservices', { service_ok: nagios_services_count[0], service_warning: nagios_services_count[1], service_critical: nagios_services_count[2], service_unknown: nagios_services_count[3], service_pending: nagios_services_count[4], servicestatus: servicestatus })
-    send_event('messages', { nagios_hosts: nagios_hosts_message, nagios_services: nagios_services_message}) 
+SCHEDULER.every '20', :first_in => 0 do
+    resp = Net::HTTP.get_response(URI.parse(hostsURL))
+    jsonData = resp.body
+    hostsStatus = JSON.parse(jsonData)
 
-  else #no data from nagios server
-    send_event('nagiosxihosts', { host_up: 0, host_down: 0, host_unreachable:0, host_pending: 0, hoststatus: "error" })
-    sleep(1)
-    send_event('nagiosxiservices', {  service_ok: 0, service_warning: 0, service_critical: 0, service_unknown: 0, service_pending: 0,servicestatus: "error" })
-    send_event('messages', { nagios_hosts: nagios_hosts_message, nagios_services: nagios_services_message}) 
-  end
+    resp2 = Net::HTTP.get_response(URI.parse(servicesURL))
+    jsonData2 = resp2.body
+    servicesStatus = JSON.parse(jsonData2)
+
+    nagios_hosts_count = {"up" => 0, "down" => 0, "unreachable" => 0, "scheduled" => 0, "ack" => 0}
+    nagios_services_count = {"ok" => 0,"warning" => 0, "critical" => 0, "unknown" => 0, "pending" => 0, "ack" => 0, "scheduled" => 0}
+
+    nagios_message = ['','','','','']
+
+    nagios_hosts_highest_status = 0;
+    nagios_services_highest_status = 0;
+
+    hosts = {}
+
+    if (hostsStatus['hoststatus'].respond_to? :each and servicesStatus['servicestatus'].respond_to? :each)
+
+        hostsStatus['hoststatus'].each do |child|
+            host_current_state = child['current_state'].to_i
+            host_name = child['name']
+
+            case host_current_state
+            when 0 #up
+                nagios_hosts_count["up"] += 1
+            else
+                hosts[host_name] == nil ? hosts[host_name] = {} : nil
+                hosts[host_name]['ack'] = child['problem_acknowledged'] == "0" ? false : true
+                hosts[host_name]['scheduled'] = child['scheduled_downtime_depth'] == "0" ? false : true
+                hosts[host_name]['state'] = 1
+                hosts[host_name]['alias'] = child['alias']
+                hosts[host_name]['services'] = []
+
+                if hosts[host_name]['scheduled']
+                    nagios_hosts_count["scheduled"] += 1
+                elsif hosts[host_name]['ack']
+                    nagios_hosts_count["ack"] +=1
+                else
+                    nagios_hosts_count["down"] +=1
+                    nagios_hosts_highest_status = 3
+                end
+            end
+        end
+
+        servicesStatus['servicestatus'].each do |child|
+            service = {}
+            service['ack'] = child['problem_acknowledged'] == "0" ? false : true
+            service['scheduled'] = child['scheduled_downtime_depth'] == "0" ? false : true
+            service['state'] = child['current_state'].to_i
+            service['name'] = child['display_name'].to_s
+            service['state'] == 3 ?  service['text'] = 'Check Pending' : service['text'] = child['status_text'].to_s
+                
+            
+
+            host_name = child['host_name']
+            host_alias = child['host_alias']
+
+            if service['state'] == 0
+                nagios_services_count["ok"] += 1
+            else
+                hosts[host_name] == nil ? hosts[host_name] = {} : nil
+                hosts[host_name]['ack'] == nil ? hosts[host_name]['ack'] = 0 : nil
+                hosts[host_name]['scheduled'] == nil ? hosts[host_name]['scheduled'] = 0 : nil
+                hosts[host_name]['state'] == nil ? hosts[host_name]['state'] = 0 : nil
+                hosts[host_name]['alias'] == nil ? hosts[host_name]['alias'] = host_alias : nil
+                hosts[host_name]['services'] == nil ? hosts[host_name]['services'] = [] : nil
+                hosts[host_name]['services'].push(service)
+
+                if service['scheduled']
+                    nagios_services_count["scheduled"] += 1
+                elsif service['ack']
+                    nagios_services_count["ack"] +=1
+                end
+
+                case service['state']                    
+                when 1 #warning
+                    nagios_services_count["warning"] += 1
+                    if !service['ack'] and !service['scheduled'] and nagios_services_highest_status < 3
+                        nagios_services_highest_status = 3
+                    end
+                when 2 #critical
+                    nagios_services_count["critical"] += 1
+                    if !service['ack'] and !service['scheduled'] and nagios_services_highest_status < 4
+                        nagios_services_highest_status = 4
+                    end
+                when 3 #unknown
+                    nagios_services_count["unknown"] += 1
+                    if !service['ack'] and !service['scheduled'] and nagios_services_highest_status < 2
+                        nagios_services_highest_status = 2
+                    end
+                else #pending
+                    nagios_services_count["pending"] += 1
+                    if !service['ack'] and !service['scheduled'] and nagios_services_highest_status < 1
+                        nagios_services_highest_status = 1
+                    end
+                end
+            end
+        end
+
+        hosts.each do |key, host|
+            msg_tmp = ''
+            msg_level = 0
+            if host['state'] != 0
+                msg_level = getServiceLevel(4,host['scheduled'],host['ack'])
+                msg_tmp = "<p class='single-message-header'><i class='fa " + getLevelIcon(msg_level) + "'></i><span class='servername'>" + host['alias'].to_s + "</span><span class='detail'>HOST DOWN</span></p>"
+            else
+                services_shown = 0
+                host['services'].each do |service|
+                    service_level = getServiceLevel(service['state'],service['scheduled'],service['ack'])
+                    msg_level < service_level ? msg_level = service_level : nil
+                    if services_shown < 3
+                        msg_tmp += "<p class='single-message-body'><i class='fa " + getLevelIcon(service_level) + "'></i><span>" + service['name'] + " - " + service['text'] + "</span></p>"
+                    elsif services_shown == 3
+                        msg_tmp += "<p class='single-message-body'><i class='fa " + getLevelIcon(4) + "'></i><span>... other services ...</span></p>"
+                    end
+                    services_shown+=1
+                end
+                msg_tmp = "<p class='single-message-header'><i class='fa " + getLevelIcon(msg_level) + "'></i><span class='servername'>" + host['alias'].to_s + "</span></p>" + msg_tmp
+            end
+            nagios_message[msg_level] += msg_tmp
+        end
+
+        send_event('messages', { nagios_message: nagios_message[4] + nagios_message[3] + nagios_message[2] + nagios_message[1] + nagios_message[0]}) 
+
+
+        hoststatus = nagios_hosts_highest_status == 3 ? "red" : (nagios_hosts_highest_status == 2 ? "yellow" : (nagios_hosts_highest_status > 0 ? "grey" : "green"))
+        servicestatus = nagios_services_highest_status == 4 ? "red" : (nagios_services_highest_status == 3 ? "yellow" : (nagios_services_highest_status > 0 ? "grey" : "green"))
+
+        send_event('nagiosxihosts', { host_up: nagios_hosts_count['up'], host_down: nagios_hosts_count['down'], host_unreachable: nagios_hosts_count['unreachable'], hoststatus: hoststatus })
+        send_event('nagiosxiservices', { service_ok: nagios_services_count['ok'], service_warning: nagios_services_count['warning'], service_critical: nagios_services_count['critical'], service_unknown: nagios_services_count['unknown'],servicestatus: servicestatus })
+        
+
+
+    else #no data from nagios server
+        send_event('nagiosxihosts', { host_up: 0, host_down: 0, host_unreachable:0, hoststatus: "error" })
+        sleep(1)
+        send_event('nagiosxiservices', {    service_ok: 0, service_warning: 0, service_critical: 0, service_unknown: 0, service_pending: 0,servicestatus: "error" })
+        send_event('messages', { nagios_message: nagios_message}) 
+
+    end
 
 end
